bbc681f42f6037455507771e03d327e1d5d5621c
OlegMoshkovich <oleg.mosh@gmail.com>
2014-07-10 07:42:50 -0400

    minor change to the inspection controller

--- app/controllers/inspection_reports_controller.rb
+++ app/controllers/inspection_reports_controller.rb
@@ -1,5 +1,5 @@
 class InspectionReportsController < ApplicationController
-	#before_action :authenticate_user!
+	before_action :authenticate_user!
 
 	def index
 		@inspection_reports = InspectionReport.all
